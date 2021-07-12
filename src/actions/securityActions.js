8ec05fd4cc5d164798c56d535f4eca5af8ce433b
Midhun Balan <midhun.balan@anywhere.co>
2021-07-12 12:14:59 +0530

    added security changes

---
+++ src/actions/securityActions.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import errorReducer from "../reducers/errorReducer";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from 'jwt-decode'
+
+export const createNewUser = (newUser, history) => async dispatch => {
+    try {
+      await axios.post("/api/users/register", newUser);
+      history.push("/login");
+      dispatch({
+        type: GET_ERRORS,
+        payload: {}
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+  };
+
+/*
+1. post  login request
+2. extract token from res.data
+3. store the token in the localStorage
+4. set our token in header - secure with your token
+5. decode the token on React
+6. dispatch to our securityReducer
+*/ 
+  export const login = LoginRequest => async dispatch =>{
+      try{
+          const res = await axios.post("/api/users/login", LoginRequest)
+          const {token} = res.data;
+          localStorage.setItem("jwtToken", token);
+          setJWTToken(token);
+          const decoded = jwt_decode(token);
+          dispatch({
+              type: SET_CURRENT_USER,
+              payload: decoded
+          })
+      }catch(err){
+          dispatch({
+              type: GET_ERRORS,
+              payload: err.response.data
+          })
+      }
+  }
+
+  export const logout = () => dispatch =>{
+    localStorage.removeItem("jwtToken");
+    setJWTToken(false);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: {}
+  })
+  }
